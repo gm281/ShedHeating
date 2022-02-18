85282fbbda4a38a7f638b8905e6b32f151548cd4
Grzegorz Miłoś <gmilos@apple.com>
2022-02-18 15:55:22 +0000

    Make logging friendly to `docker run --detach`

--- common.sh
+++ common.sh
@@ -2,15 +2,10 @@
 
 CURRENT_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-RED=$(tput setaf 1)
-YELLOW=$(tput setaf 3)
-CYAN=$(tput setaf 6)
-BRIGHT=$(tput bold)
-NORMAL=$(tput sgr0)
-log() { printf -- "${BRIGHT}${CYAN}** %s\n${NORMAL}" "$*" >&2; }
-log_notable() { printf -- "${BRIGHT}${RED}** %s\n${NORMAL}" "$*" >&2; }
-warn() { printf -- "${BRIGHT}${YELLOW}** WARN: %s\n${NORMAL}" "$*" >&2; }
-error() { printf -- "${BRIGHT}${RED}** ERROR: %s\n${NORMAL}" "$*" >&2; }
+log() { printf -- "%s\n" "$*" >&2; }
+log_notable() { printf -- "** %s\n" "$*" >&2; }
+warn() { printf -- "** WARN: %s\n" "$*" >&2; }
+error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
 trap 'error "command failed (line ${LINENO})"' ERR
